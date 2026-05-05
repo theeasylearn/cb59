d4433e2d905f31a22b0e646164356440da4f9bff
theeasylearn <theeasylearn@gmail.com>
2026-05-05 13:59:14 +0530

    loop

---
+++ 29_loop_4.c
@@ -0,0 +1,21 @@
+/* write a program to make sum of all digits in given amount
+   amount = 1 2 3 4 7 9
+   process =  1 + 2 + 3 + 4 + 7 + 9
+   output = 26
+*/
+#include <stdio.h>
+void main()
+{
+    int amount, sum = 0, last_digit;
+
+    printf("Enter amount");
+    scanf("%d", &amount);
+    
+    while (amount > 0)
+    {
+        last_digit = amount % 10; // 9
+        sum = sum + last_digit;   // 9
+        amount = amount / 10;     // 12347
+    }
+    printf("sum %d", sum);
+}
